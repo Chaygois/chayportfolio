91407250adc7a192a9ec67849022b6b9ce49372f
ChayGois <you@example.com>
2024-02-25 05:32:53 -0300

    up

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -1,61 +1,44 @@
 import React from "react";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
-
 import Card from "../common/card";
-
 import "./styles/works.css";
 
 const Works = () => {
-	return (
-		<div className="works">
-			<Card
-				icon={faBriefcase}
-				title="Work"
-				body={
-					<div className="works-body">
-						<div className="work">
-							<img
-								src="https://cdn.discordapp.com/attachments/1120323557491212288/1208022599846858812/Screenshot_2024-02-16_at_09-10-12_Solucoes_em_Blockchain_GoLedger._Gerenciando_de_APIs_com_protocolo_da_confianca._Gerenciando_de_APIs_em_blockchains_permissionados_para_rastreabilidade_gestao_de_documentos_ide....png?ex=65e1c5b7&is=65cf50b7&hm=c656ac4217f548e0274e8bec2fb07980b944dc1e9066a0df999cdb6e247aa107&"
-								alt="qa"
-								className="work-image"
-							/>
-							<div className="work-title">GoLedger</div>
-							<div className="work-subtitle">
-								QA
-							</div>
-							<div className="work-duration">2023 - 2024</div>
-						</div>
-						<div className="work">
-							<img
-								src="https://cdn.discordapp.com/attachments/1120323557491212288/1208021297582448671/Screenshot_2024-02-16_at_09-00-23_1683034743302_imagem_JPEG_200_200_pixels.png?ex=65e1c481&is=65cf4f81&hm=3960e0429aa0d994b52cd191fa69c86a6b832c36f618ffd576f06a7a58e2a3a1&"
-								alt="qa"
-								className="work-image"
-							/>
-							<div className="work-title">Checkmate Property</div>
-							<div className="work-subtitle">
-								QA Engineer
-							</div>
-							<div className="work-duration">2022 - 2023</div>
-						</div>
+  return (
+    <div className="works">
+      <Card
+        icon={faBriefcase}
+        title="Work"
+        body={
+          <div className="works-body">
 
-						<div className="work">
-							<img
-								src="https://scontent.ffor13-1.fna.fbcdn.net/v/t39.30808-6/362954584_678332664327976_6200431304471991411_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=ThbaalXuIuoAX8TFOeW&_nc_ht=scontent.ffor13-1.fna&oh=00_AfDE_vrhUMEoJe3O0aPEb8VgDWjdMrU9DxanvQ9zYJZLkQ&oe=65D4D87B"
-								alt="BNB"
-								className="work-image"
-							/>
-							<div className="work-title">BANCO DO NORDESTE</div>
-							<div className="work-subtitle">
-								Front end Junior
-							</div>
-							<div className="work-duration">2021 - 2022</div>
-						</div>
-						
-					</div>
-				}
-			/>
-		</div>
-	);
+            <div className="work">
+              <img src="/qa33.png" alt="goledgerQA" className="qa1" />
+              <div className="work-title">GoLedger</div>
+              <div className="work-subtitle">QA</div>
+              <div className="work-duration">2023 - 2024</div>
+            </div>
+
+            <div className="work">
+              <img src="/qa1.png" alt="goledgerQA" className="qa2" />
+
+              <div className="work-title">Checkmate Property</div>
+              <div className="work-subtitle">QA Engineer</div>
+              <div className="work-duration">2022 - 2023</div>
+            </div>
+
+            <div className="work">
+              
+               <img src="/front2.png" alt="goledgerQA" className="qa3" />
+              <div className="work-title">BANCO DO NORDESTE</div>
+              <div className="work-subtitle">Front end Junior</div>
+              <div className="work-duration">2021 - 2022</div>
+            </div>
+          </div>
+        }
+      />
+    </div>
+  );
 };
 
 export default Works;
--- src/data/articles.js
+++ src/data/articles.js
@@ -1,16 +1,13 @@
-import React from "react";
+import React from 'react'
 
-function article_1() {
-	return {
-		date: "10 Jan 2023",
-		title: "Unlocking the Secrets of Behavior-Driven Development (BDD)",
-		description:
-			"In the dynamic world of software development, it is crucial to adopt practices that promote effective collaboration, clear understanding of requirements, and the delivery of high-quality software",
-		keywords: [
-			"The Benefits of Cloud Computing",
-			"Tharindu",
-		],
-		style: `
+function article_1 () {
+  return {
+    date: '10 Jan 2023',
+    title: 'Unlocking the Secrets of Behavior-Driven Development (BDD)',
+    description:
+      'In the dynamic world of software development, it is crucial to adopt practices that promote effective collaboration, clear understanding of requirements, and the delivery of high-quality software',
+    keywords: ['The Benefits of Cloud Computing', 'Tharindu'],
+    style: `
 				.article-content {
 					display: flex;
 					flex-direction: column;
@@ -22,160 +19,195 @@ function article_1() {
 					outline: 2px solid red;
 				}
 				`,
-		body: (
-			<React.Fragment>
-		<div className="article-content">
-    <img src="/qa.jpg" alt="Sua Foto QA" className="qa-photo" />
-	<div className='text' id='texto-animado'>
-                  <h3>
-                    In the dynamic realm of software development, it is crucial
-                    to adopt practices that foster effective collaboration, a
-                    clear understanding of requirements, and the delivery of
-                    high-quality software. An approach that stands out in this
-                    scenario is Behavior-Driven Development (BDD). In this
-                    article, we will explore the fundamentals of BDD, its
-                    significance in enhancing communication among teams, and how
-                    to transform requirements into successful scenarios.
-                    <h2> What is BDD?</h2>
-                    Behavior-Driven Development (BDD) is an agile development
-                    methodology that focuses on effective communication among
-                    different stakeholders, including developers, testers, and
-                    stakeholders. The main idea behind BDD is to create a shared
-                    understanding of requirements through concrete examples
-                    expressed in natural language.
-                    <h2>The Pillars of BDD: Given, When, Then</h2>
-                    In BDD, test scenarios are structured around three
-                    fundamental parts: Given, When, and Then. Given: Defines the
-                    initial state or preconditions of the system before the
-                    action is executed. When: Describes the action or event that
-                    triggers the behavior being tested. Then: Specifies the
-                    expected result or behavior that the system should exhibit
-                    in response to the action. This simple structure, expressed
-                    in natural language, facilitates the creation of test
-                    scenarios understandable to everyone, regardless of their
-                    technical expertise.
-                    <h2>
-                      Essential Books to Deepen Your Understanding of BDD:
-                    </h2>
-                    "BDD in Action" - John Ferguson Smart: "When implementing
-                    BDD, it is crucial to understand that each test scenario is
-                    a narrative. Each step, expressed in the Given, When, Then
-                    format, tells the story of how the system should behave."
-                    "Specification by Example" - Gojko Adzic: "Specification by
-                    example is more than a technique; it is a philosophy. We
-                    should strive to express requirements using tangible
-                    examples that everyone can understand, regardless of their
-                    technical expertise." "The Cucumber Book" - Matt Wynne and
-                    Aslak Hellesøy: "Automating BDD tests with Cucumber is like
-                    translating a story into a language the computer can
-                    understand. Each line of code should reflect the clarity and
-                    intention of the story being told." "BDD for Testers" -
-                    Samantha Connelly: "Testers play a crucial role in bridging
-                    the gap between requirements and implementation. Automating
-                    BDD tests is not just a technical task but a contribution to
-                    ensuring that the language of tests is understood by
-                    everyone involved in the project."
-                    <h2>Benefits of BDD: A Comprehensive Perspective</h2>
-                    Shared Understanding: BDD promotes a shared understanding of
-                    requirements, ensuring that all stakeholders have the same
-                    vision of what is being built. Enhanced Collaboration: By
-                    involving developers, testers, and stakeholders from the
-                    beginning, BDD facilitates collaboration, reducing
-                    misunderstandings and improving team efficiency. Living
-                    Documentation: BDD scenarios serve as a form of living
-                    documentation, always updated and aligned with the actual
-                    behavior of the system.
-                    <h2>How to Implement BDD in Your Project</h2>
-                    Collaboration from the Beginning: Include all stakeholders
-                    in discussions from the project's inception to ensure a
-                    shared understanding of requirements. Write Clear and
-                    Concise Scenarios: Use the Given, When, Then structure to
-                    create test scenarios that are understandable to everyone.
-                    Automate BDD Tests: Use tools like Cucumber to automate the
-                    execution of BDD scenarios, ensuring continuous validation
-                    of the system's behavior. Continuous Iteration: Review and
-                    adjust scenarios as the project evolves, ensuring that
-                    living documentation remains accurate.
-                    <h2> Conclusion</h2>
-                    Behavior-Driven Development (BDD) is not just a methodology
-                    but a mindset shift that promotes a collaborative and
-                    user-centered approach. By incorporating BDD into
-                    development processes, teams can not only improve software
-                    quality but also foster a deeper understanding of
-                    requirements, ensuring project success. Embrace BDD as a
-                    powerful tool to transform requirements into successful
-                    scenarios, paving the way for more effective and
-                    results-oriented software development.
-                  </h3>
-</div>
-</div>
-
-			</React.Fragment>
-		),
-	};
+    body: (
+      <React.Fragment>
+        <div className='article-content'>
+          <img src='/qa.jpg' alt='Sua Foto QA' className='qa-photo' />
+          <div className='text' id='texto-animado'>
+            <h3>
+              In the dynamic realm of software development, it is crucial to
+              adopt practices that foster effective collaboration, a clear
+              understanding of requirements, and the delivery of high-quality
+              software. An approach that stands out in this scenario is
+              Behavior-Driven Development (BDD). In this article, we will
+              explore the fundamentals of BDD, its significance in enhancing
+              communication among teams, and how to transform requirements into
+              successful scenarios.
+              <h2> What is BDD?</h2>
+              Behavior-Driven Development (BDD) is an agile development
+              methodology that focuses on effective communication among
+              different stakeholders, including developers, testers, and
+              stakeholders. The main idea behind BDD is to create a shared
+              understanding of requirements through concrete examples expressed
+              in natural language.
+              <h2>The Pillars of BDD: Given, When, Then</h2>
+              In BDD, test scenarios are structured around three fundamental
+              parts: Given, When, and Then. Given: Defines the initial state or
+              preconditions of the system before the action is executed. When:
+              Describes the action or event that triggers the behavior being
+              tested. Then: Specifies the expected result or behavior that the
+              system should exhibit in response to the action. This simple
+              structure, expressed in natural language, facilitates the creation
+              of test scenarios understandable to everyone, regardless of their
+              technical expertise.
+              <h2>Essential Books to Deepen Your Understanding of BDD:</h2>
+              "BDD in Action" - John Ferguson Smart: "When implementing BDD, it
+              is crucial to understand that each test scenario is a narrative.
+              Each step, expressed in the Given, When, Then format, tells the
+              story of how the system should behave." "Specification by Example"
+              - Gojko Adzic: "Specification by example is more than a technique;
+              it is a philosophy. We should strive to express requirements using
+              tangible examples that everyone can understand, regardless of
+              their technical expertise." "The Cucumber Book" - Matt Wynne and
+              Aslak Hellesøy: "Automating BDD tests with Cucumber is like
+              translating a story into a language the computer can understand.
+              Each line of code should reflect the clarity and intention of the
+              story being told." "BDD for Testers" - Samantha Connelly: "Testers
+              play a crucial role in bridging the gap between requirements and
+              implementation. Automating BDD tests is not just a technical task
+              but a contribution to ensuring that the language of tests is
+              understood by everyone involved in the project."
+              <h2>Benefits of BDD: A Comprehensive Perspective</h2>
+              Shared Understanding: BDD promotes a shared understanding of
+              requirements, ensuring that all stakeholders have the same vision
+              of what is being built. Enhanced Collaboration: By involving
+              developers, testers, and stakeholders from the beginning, BDD
+              facilitates collaboration, reducing misunderstandings and
+              improving team efficiency. Living Documentation: BDD scenarios
+              serve as a form of living documentation, always updated and
+              aligned with the actual behavior of the system.
+              <h2>How to Implement BDD in Your Project</h2>
+              Collaboration from the Beginning: Include all stakeholders in
+              discussions from the project's inception to ensure a shared
+              understanding of requirements. Write Clear and Concise Scenarios:
+              Use the Given, When, Then structure to create test scenarios that
+              are understandable to everyone. Automate BDD Tests: Use tools like
+              Cucumber to automate the execution of BDD scenarios, ensuring
+              continuous validation of the system's behavior. Continuous
+              Iteration: Review and adjust scenarios as the project evolves,
+              ensuring that living documentation remains accurate.
+              <h2> Conclusion</h2>
+              Behavior-Driven Development (BDD) is not just a methodology but a
+              mindset shift that promotes a collaborative and user-centered
+              approach. By incorporating BDD into development processes, teams
+              can not only improve software quality but also foster a deeper
+              understanding of requirements, ensuring project success. Embrace
+              BDD as a powerful tool to transform requirements into successful
+              scenarios, paving the way for more effective and results-oriented
+              software development.
+            </h3>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
-function article_2() {
-	return {
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
-		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
-		style: ``,
-		keywords: [
-			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
-		],
-		body: (
-			<React.Fragment>
-				<h1>Cjlnhoujhuihoiuhnoihnilnhonoinoinininoipnoinoinkonknokmjkilmoilmn</h1>
-			</React.Fragment>
-		),
-	};
+function article_2 () {
+  return {
+    date: '7 May 2023',
+    title: 'Software Testing Techniques: A Comprehensive Approach',
+    description: 'Software testing plays a crucial role in ensuring the quality and reliability of software systems. ...',
+    style: ``,
+    keywords: [
+      ''
+    ],
+    body: (
+      <React.Fragment>
+        <h3>
+          Software testing plays a crucial role in ensuring the quality and
+          reliability of software systems. Among the essential practices in the
+          testing field, various techniques are employed to detect defects,
+          verify compliance with requirements, and ensure that the software
+          meets user expectations. In this article, we will explore some of the
+          key testing techniques, ranging from black-box methods to more
+          code-oriented approaches. 1. Black-Box Testing Techniques Black-box
+          testing techniques focus on analyzing the software's behavior without
+          considering its internal structure. Among these techniques, the
+          following are noteworthy: 1.1 Equivalence Partitioning: Description:
+          Divides data into partitions, assuming that elements in the same
+          partition are processed similarly. Learning Objective (Example):
+          Distinguish between black-box, white-box, and experience-based testing
+          techniques. 1.2 Boundary Value Analysis: Description: Tests the limits
+          of equivalence partitions, focusing on values at the extremes.
+          Learning Objective (Example): Use boundary value analysis to derive
+          test cases. 1.3 Decision Table Testing: Description: Uses tables to
+          test different combinations of conditions and their associated
+          results. Learning Objective (Example): Employ acceptance test-driven
+          development (ATDD) to derive test cases. 1.4 State Transition Testing:
+          Description: Models the system's behavior in terms of states and
+          transitions. Learning Objective (Example): Explain how to write user
+          stories in collaboration with developers. 2. White-Box Testing
+          Techniques White-box techniques aim to examine the internal structure
+          of the source code for comprehensive coverage. Some of them include:
+          2.1 Statement Testing: Description: Focuses on executing executable
+          statements in the code. Learning Objective (Example): Explain
+          statement testing. 2.2 Branch Testing: Description: Models and tests
+          decisions made in the code, covering all branches. Learning Objective
+          (Example): Explain branch testing. 3. Experience-Based Testing
+          Techniques These techniques rely on testers' knowledge and experience
+          to identify defects and critical areas. Some of them include: 3.1
+          Error Guessing: Description: Predicts possible errors based on past
+          experience. Learning Objective (Example): Explain error guessing. 3.2
+          Exploratory Testing: Description: Modeled, executed, and evaluated
+          simultaneously, ideal for scenarios with inadequate specifications.
+          Learning Objective (Example): Use exploratory testing in environments
+          with limited specifications. 3.3 Checklist-Based Testing: Description:
+          Tests designed to cover specific conditions from a pre-defined
+          checklist. Learning Objective (Example): Create and execute
+          checklist-based tests. Conclusion The choice of testing techniques
+          depends on the project context, requirements, and available resources.
+          Combining black-box and white-box approaches, along with incorporating
+          experience-based techniques, provides a robust strategy for ensuring
+          software quality. By understanding and applying these techniques,
+          testers play a crucial role in delivering reliable and effective
+          software.
+        </h3>
+      </React.Fragment>
+    )
+  }
 }
-function article_3() {
-	return {
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
-		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
-		style: ``,
-		keywords: [
-			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
-		],
-		body: (
-			<React.Fragment>
-				<h1>Content of article 2</h1>
-			</React.Fragment>
-		),
-	};
+function article_3 () {
+  return {
+    date: '7 May 2023',
+    title: 'Artificial Intelligence in Healthcare',
+    description:
+      'AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.',
+    style: ``,
+    keywords: [
+      'Artificial Intelligence in Healthcare',
+      'Tharindu',
+      'Tharindu N',
+      'Tharindu Nayanajith'
+    ],
+    body: (
+      <React.Fragment>
+        <h1>Content of article 2</h1>
+      </React.Fragment>
+    )
+  }
 }
-function article_4() {
-	return {
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
-		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
-		style: ``,
-		keywords: [
-			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
-		],
-		body: (
-			<React.Fragment>
-				<h1>Content of article 2</h1>
-			</React.Fragment>
-		),
-	};
+function article_4 () {
+  return {
+    date: '7 May 2023',
+    title: 'Artificial Intelligence in Healthcare',
+    description:
+      'AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.',
+    style: ``,
+    keywords: [
+      'Artificial Intelligence in Healthcare',
+      'Tharindu',
+      'Tharindu N',
+      'Tharindu Nayanajith'
+    ],
+    body: (
+      <React.Fragment>
+        <h1>Content of article 2</h1>
+      </React.Fragment>
+    )
+  }
 }
 
-const myArticles = [article_1, article_2 ,article_3, article_4];
+const myArticles = [article_1, article_2, article_3, article_4]
 
-export default myArticles;
+export default myArticles
